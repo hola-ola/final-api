acd52d3f6400ae72a46b38d722b48c24bd0f7674
Aleksandra Porada <aleksandra.porada92@gmail.com>
2021-06-01 21:46:02 +0200

    Started with Cloudinary set-up and listing routing

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 const router = require("express").Router();
-const authRoutes = require("./auth");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
 
+/* AUTH routes */
+const authRoutes = require("./auth");
 router.use("/auth", authRoutes);
+
+/* LISTING routes */
+const listingRoutes = require("./listing");
+router.use("/listing", listingRoutes);
 
 module.exports = router;
