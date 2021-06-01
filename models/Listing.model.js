1bb70931b7d3fb2261b1caad6ef030e2e1c6909d
Aleksandra Porada <aleksandra.porada92@gmail.com>
2021-06-01 20:50:35 +0200

    Listing model

---
+++ models/Listing.model.js
@@ -0,0 +1,100 @@
+const { Schema, model } = require("mongoose");
+
+const LENGTH_OF_STAY_ENUM = require("../utils/consts");
+const LISTING_TYPE_ENUM = require("../utils/consts");
+
+const listingSchema = new Schema(
+  {
+    title: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    lengthOfStay: {
+      type: String,
+      required: true,
+      enum: LENGTH_OF_STAY_ENUM,
+    },
+    type: {
+      type: String,
+      required: true,
+      enum: LISTING_TYPE_ENUM,
+    },
+    numberOfSleepingSpots: {
+      type: String,
+      required: true,
+    },
+    generalDescription: {
+      type: String,
+      required: true,
+    },
+    kitchenEquipment: {
+      type: [String],
+      required: true,
+    },
+    bathroomEquipment: {
+      type: [String],
+      required: true,
+    },
+    workSetup: {
+      type: [String],
+      required: true,
+    },
+    Accessability: {
+      type: [String],
+      required: true,
+    },
+    forSmokers: {
+      type: Boolean,
+      required: true,
+    },
+    kidsWelcome: {
+      type: Boolean,
+      required: true,
+    },
+    petsWelcome: {
+      type: Boolean,
+      required: true,
+    },
+    spaceOutside: {
+      type: Boolean,
+      required: true,
+    },
+    extraRemarks: {
+      type: String,
+      required: true,
+    },
+    ambienceDescription: {
+      type: String,
+      required: true,
+    },
+    imagesGallery: {
+      type: [String],
+      required: true,
+    },
+    Availability: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Listing = model("Listing", listingSchema);
+
+module.exports = Listing;
---
+++ utils/consts.js
@@ -0,0 +1,12 @@
+const LENGTH_OF_STAY_ENUM = [
+  "Less than a week",
+  "Weekend",
+  "Up to two weeks",
+  "Up to a month",
+  "Long stay",
+];
+
+const LISTING_TYPE_ENUM = ["House", "Apartment", "Room in a shared place"];
+
+module.exports = LENGTH_OF_STAY_ENUM;
+module.exports = LISTING_TYPE_ENUM;
