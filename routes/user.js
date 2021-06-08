81839e91756a9873a00f8408c8e80398f13ca4ba
Aleksandra Porada <aleksandra.porada92@gmail.com>
2021-06-08 21:38:00 +0200

    We can now delete the user.

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,14 @@
 const router = require("express").Router();
 
-// Require the Listing model in order to interact with the database
+// Require the models in order to interact with the database
 const User = require("../models/User.model");
+const Session = require("../models/Session.model");
 
 // Require necessary (isLoggedIn) middleware in order to control access to specific routes
 const isLoggedIn = require("../middleware/isLoggedIn");
 
 // GET User profile page
 router.get("/:username", isLoggedIn, (req, res) => {
-  console.log("We are here:", req.params.username);
   User.findOne({
     username: req.params.username,
   })
@@ -29,25 +29,38 @@ router.get("/:username", isLoggedIn, (req, res) => {
 
 // DELETE User profile
 router.get("/:username/delete", isLoggedIn, (req, res) => {
-  console.log("Deleting the user – message from the server");
-  if (req.match.params.username === req.params.user.username) {
-    User.findByIdAndDelete({
-      username: req.match.params.username,
-    })
-      .then((foundUser) => {
-        if (!foundUser) {
-          return res.status(400).json({
-            errorMessage: "User not found!",
-            key: "user-delete",
+  User.findById(req.user._id)
+    .then((foundUser) => {
+      // Check if user exists
+      if (!foundUser) {
+        return res.status(404).json({ errorMessage: "User not found!" });
+      }
+      // Check is user is the owner of the account
+      if (foundUser.username !== req.params.username) {
+        return res.status(404).json({
+          errorMessage: "You're not the owner of this account!",
+        });
+      }
+      // We know that user exists and is the owner of the account
+      return User.findByIdAndDelete(foundUser._id)
+        .then((deletedUser) => {
+          return Session.findOneAndDelete({
+            user: deletedUser._id,
           });
-        }
-        res.json(true);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.json(500).json({ errorMessage: err.message });
-      });
-  }
+        })
+        .catch((err) => {
+          console.log(err);
+          res.json(500).json({ errorMessage: err.message });
+        });
+    })
+    .then(() => {
+      console.log("We removed the session and the user!");
+      res.json(true);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json(500).json({ errorMessage: err.message });
+    });
 });
 
 module.exports = router;
