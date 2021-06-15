04f6b28ecfaeb67497b8a8cb50923674db21a230
Aleksandra Pietraszek <ola.pietraszek@gmail.com>
2021-06-15 12:07:49 +0200

    add user to the listing

--- routes/listings.js
+++ routes/listings.js
@@ -1,88 +1,65 @@
 const router = require("express").Router();
 
-// Require the Listing model in order to interact with the database
 const Listing = require("../models/Listing.model");
-
-// Require necessary (isLoggedIn) middleware in order to control access to specific routes
-const isLoggedIn = require("../middleware/isLoggedIn");
-
 const User = require("../models/User.model");
 
+const isLoggedIn = require("../middleware/isLoggedIn");
+
 router.post("/create", isLoggedIn, (req, res) => {
-  User.findOne({ _id: req.user._id }).then((foundUser) => {
-    if (foundUser.userListing) {
-      return res
-        .status(400)
-        .json({ errorMessage: "The user already created a listing" });
-    }
+  User.findOne({ _id: req.user._id })
+    .then((foundUser) => {
+      if (foundUser.userListing) {
+        return res
+          .status(400)
+          .json({ errorMessage: "The user already created a listing" });
+      }
 
-    const {
-      title,
-      country,
-      city,
-      lengthOfStay,
-      type,
-      numberOfSleepingSpots,
-      generalDescription,
-      kitchenEquipment,
-      bathroomEquipment,
-      accessability,
-      smokersWelcome,
-      kidsWelcome,
-      petsWelcome,
-      spaceOutside,
-      extraRemarks,
-      ambienceLabels,
-      imagesGallery,
-      availability,
-    } = req.body.formValues;
+      const {
+        title,
+        country,
+        city,
+        lengthOfStay,
+        type,
+        numberOfSleepingSpots,
+        generalDescription,
+        kitchenEquipment,
+        bathroomEquipment,
+        accessability,
+        smokersWelcome,
+        kidsWelcome,
+        petsWelcome,
+        spaceOutside,
+        extraRemarks,
+        ambienceLabels,
+        imagesGallery,
+        availability,
+      } = req.body.formValues;
 
-    if (!title || !country || !city) {
-      return res
-        .status(400)
-        .json({ errorMessage: "Please fill in all required fields" });
-    }
+      if (!title || !country || !city) {
+        return res
+          .status(400)
+          .json({ errorMessage: "Please fill in all required fields" });
+      }
 
-    Listing.create({
-      ...req.body.formValues,
-      owner: foundUser,
-    })
-      .then((createdListing) => {
-        res.json({ listing: createdListing });
+      Listing.create({
+        ...req.body.formValues,
+        owner: foundUser,
       })
-      .catch((err) => {
-        res.status(500).json({ errorMessage: err.message });
-      });
-
-    //koniec UserFindOne
-  });
-
-  // Listing.create({
-  //   ...req.body.formValues,
-  //   owner: req.user._id,
-  // })
-  //   .then((createdListing) => {
-  //     User.findByIdAndUpdate(
-  //       req.user._id,
-  //       {
-  //         $push: { userListing: createdListing },
-  //       },
-  //       { new: true }
-  //     );
-  //   })
-  //   .then(() => {
-  //     console.log(createdListing);
-  //     // res.json({ listing: createdListing });
-  //   })
-  //   .catch((err) => {
-  //     res.status(500).json({ errorMessage: err.message });
-  //   });
-
-  //koniec RouterPost
+        .then((createdListing) => {
+          res.json({ listing: createdListing });
+        })
+        .catch((err) => {
+          res.status(500).json({ errorMessage: err.message });
+        });
+    })
+    .catch((err) => {
+      res.status(500).json({ errorMessage: err.message });
+    });
 });
 
 router.get("/:listingId", isLoggedIn, (req, res) => {
   Listing.findOne({ _id: req.params.listingId })
+    .populate("owner")
     .then((foundListing) => {
       res.json({ listing: foundListing });
     })
@@ -98,7 +75,6 @@ router.get("/:listingId/edit", isLoggedIn, (req, res) => {
       res.json({ listing: foundListing });
     })
     .catch((err) => {
-      // console.log(err);
       res.json(500).json({ errorMessage: err.message });
     });
 });
@@ -122,7 +98,6 @@ router.get("/:listingId/delete", isLoggedIn, (req, res) => {
       res.json({ listing: foundListing });
     })
     .catch((err) => {
-      // console.log(err);
       res.json(500).json({ errorMessage: err.message });
     });
 });
@@ -134,7 +109,6 @@ router.get("/:listingId/removed", isLoggedIn, (req, res) => {
       console.log("The listing has been removed from the database");
     })
     .catch((err) => {
-      // console.log(err);
       res.json(500).json({ errorMessage: err.message });
     });
 });
